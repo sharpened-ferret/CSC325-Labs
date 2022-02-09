8b7a257785da571da609c4731d164b4859cdd1b7
Neil Woodhouse <clonesupreme@gmail.com>
2022-02-09 19:00:37 +0000

    Working. May need to modify to match lab-sheet expected output

--- lab1/twenty_four.py
+++ lab1/twenty_four.py
@@ -3,25 +3,25 @@ from random import randint
 
 def combine(a, b):
     values = [a+b, a*b]
-    calculations = [(a, "+", b), (a, "*", b)]
+    calculations = [(0, "+", 1), (0, "*", 1)]
 
     if b > a:
         values.append(b-a)
-        calculations.append((b, "-", a))
+        calculations.append((1, "-", 0))
     else:
         values.append(a-b)
-        calculations.append((a, "-", b))
+        calculations.append((0, "-", 1))
     if b != 0:
         values.append(a/b)
-        calculations.append((a, "/", b))
+        calculations.append((0, "/", 1))
     if a != 0:
         values.append(b/a)
-        calculations.append((b, "/", a))
+        calculations.append((1, "/", 0))
     return values, calculations
 
-def calc_to_string(calc):
+def calc_to_string(calc_history, calc):
     print(calc)
-    return "{} {} {}".format(calc[0], calc[1], calc[2])
+    return "({} {} {})".format(calc_history[calc[0]], calc[1], calc_history[calc[2]])
 
 def solve(digits, goal, calcs):
 
@@ -41,7 +41,7 @@ def solve(digits, goal, calcs):
         results, result_calcs = combine(digits[0], digits[1])
         for n,result in enumerate(results):
             if result == goal:
-                return calc_to_string(result_calcs[n])
+                return calc_to_string([calcs[0], calcs[1]], result_calcs[n])
         return False
     
     # Gets all possible pairs of numbers
@@ -51,9 +51,9 @@ def solve(digits, goal, calcs):
         for i, result in enumerate(check_result):
             calc = check_calcs[i]
             
-            calc_str = calc_to_string(calc)
             new_digits = digits.copy()
             new_calcs = calcs.copy()
+            calc_history = []
 
             # Gets the positions of the digits used
             a_pos = new_digits.index(pair[0])
@@ -61,7 +61,7 @@ def solve(digits, goal, calcs):
             str_form = "Popping {}, val {}"
             print(str_form.format(pair[0], new_digits[a_pos]))
             new_digits.pop(a_pos)
-            new_calcs.pop(a_pos)
+            calc_history.append(new_calcs.pop(a_pos))
 
             b_pos = new_digits.index(pair[1])
             print(str_form.format(pair[1], new_digits[b_pos]))
@@ -69,7 +69,8 @@ def solve(digits, goal, calcs):
             new_digits.append(result)
 
             
-            new_calcs.pop(b_pos)
+            calc_history.append(new_calcs.pop(b_pos))
+            calc_str = calc_to_string(calc_history, calc)
             new_calcs.append(calc_str)
             print(new_calcs)
             print(new_digits)
