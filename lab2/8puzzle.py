7da68ca83c7c51dda01b66b9521facf149ec8cbf
Neil Woodhouse <clonesupreme@gmail.com>
2022-02-20 21:19:37 +0000

    Added initial board state object

--- lab2/8puzzle.py
+++ lab2/8puzzle.py
@@ -47,7 +47,54 @@ def generate_board(size):
         solvable = is_solvable(board)
     return board
 
+class BoardState:
+    empty_marker = -1    
+
+    def __init__(self, parent, board):
+        self.board = board
+        self.parent = parent
+        self.empty_pos = self.find_empty()
+        # self.children = self.gen_children(self.empty_pos)
+
+    def __str__(self):
+        return "parent=[{}], board={}".format(self.parent, self.board)
+    
+    def __repr__(self):
+        return self.__str__()+"\n"
+
+    def find_empty(self):
+        board_size = len(self.board)
+        empty_pos = None
+        for row in range(board_size):
+            for col in range(board_size):
+                if (self.board[row][col] == self.empty_marker):
+                    empty_pos = (row, col)
+        return empty_pos
+    
+    def gen_children(self, empty_pos):
+        children = []
+        neighbours = [
+            (empty_pos[0], empty_pos[1]+1),
+            (empty_pos[0], empty_pos[1]-1),
+            (empty_pos[0]+1, empty_pos[1]),
+            (empty_pos[0]-1, empty_pos[1])
+        ]
+        for neighbour in neighbours:
+            try:
+                board = [row[:] for row in self.board]
+                pos_val = board[neighbour[0]][neighbour[1]]
+                board[empty_pos[0]][empty_pos[1]] = pos_val
+                board[neighbour[0]][neighbour[1]] = self.empty_marker
+                children.append(BoardState(self, board))
+            except:
+                continue
+        print(children)
+
+
 
 test_puzzle = [[1,8,2], [-1, 4, 3], [7,6,5]]
-print(generate_board(3))
+test_board = generate_board(3)
+print(test_board)
+base_node = BoardState(None, test_board)
+base_node.gen_children(base_node.empty_pos)
 # print(count_inversion(test_puzzle))
