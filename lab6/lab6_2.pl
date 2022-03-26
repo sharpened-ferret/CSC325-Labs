d0ba46b467e917a137b9a9a29cd2948d768b7c64
Neil Woodhouse <clonesupreme@gmail.com>
2022-03-26 23:18:57 +0000

    lab6/task2

--- lab6/lab6_2.pl
+++ lab6/lab6_2.pl
@@ -1,31 +1,4 @@
-% % s --> np,vp.
-% s(s(A, C), A, C) :-
-%     np(A, A, B),
-%     vp(c, B, C).
-
-% % np --> det,n.
-% np(np(A, B), A, B) :-
-%     det(A, A, B),
-%     n(B, B).
-
-% % vp --> v,np.
-% vp(vp(v(A), np(C)), A, C) :-
-%     v(A, A, B),
-%     np(C, B, C). 
-
-% % det --> [the].
-% det(det(A), [the | A], A).
-% % det --> [a].
-% det(det(A), [a | A], A).
-% % n --> [woman].
-% n(n(A), [woman| A], A).
-% % n --> [man].
-% n(n(A), [man | A], A).
-% % v --> [hire].
-% v(v(A), [hire | A], A).
-
-
-% --- Clear ---
+% ---- Basic Rules ---- %
 
 % s --> np,vp.
 s(A, C) :-
@@ -56,31 +29,30 @@ v([hire | A], A).
 
 
 
+% ---- Trace Rules ---- %
 
-
-% --- Three ---
 % s --> np,vp.
 s(s(Tree1, Tree2), A, C) :-
     np(Tree1, A, B),
     vp(Tree2, B, C).
 
 % np --> det,n.
-np(np(det(A), n(B)), A, C) :-
-    det(A, B),
-    n(B, C).
+np(np(Tree1, Tree2), A, C) :-
+    det(Tree1, A, B),
+    n(Tree2, B, C).
 
 % vp --> v,np.
-vp(vp(v(A), np(B)), A, C) :-
-    v(A, B),
-    np(B, C). 
+vp(vp(Tree1, Tree2), A, C) :-
+    v(Tree1, A, B),
+    np(Tree2, B, C). 
 
 % det --> [the].
-det([the | A], A).
+det(det(the), [the | A], A).
 % det --> [a].
-det([a | A], A).
+det(det(a), [a | A], A).
 % n --> [woman].
-n([woman| A], A).
+n(n(woman), [woman| A], A).
 % n --> [man].
-n([man | A], A).
+n(n(man), [man | A], A).
 % v --> [hire].
-v([hire | A], A).
+v(v(hire), [hire | A], A).
